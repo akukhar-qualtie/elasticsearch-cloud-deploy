d14f57f4c9191f7202dc346446baabe09aabfc22
Aleksandr Kukhar <a.kukhar@qualtie.com>
2021-02-12 23:28:11 +0000

    add new output for elk module

--- terraform-aws/outputs.tf
+++ terraform-aws/outputs.tf
@@ -2,6 +2,10 @@ output "clients_dns" {
   value = aws_lb.elasticsearch-alb.*.dns_name
 }
 
+output "clients_alb_zone_id" {
+  value = aws_lb.elasticsearch-alb.*.zone_id
+}
+
 output "vm_password" {
   value = random_string.vm-login-password.result
 }
