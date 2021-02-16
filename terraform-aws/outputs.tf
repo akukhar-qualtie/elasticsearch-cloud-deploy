971b6e21586887064f7c9b489a3829e4c27391bb
Aleksandr Kukhar <a.kukhar@qualtie.com>
2021-02-16 20:03:11 +0000

    add clients security group id to output

--- terraform-aws/outputs.tf
+++ terraform-aws/outputs.tf
@@ -10,6 +10,10 @@ output "clients_alb_zone_id" {
   value = aws_lb.elasticsearch-alb.*.zone_id
 }
 
+output "clients_sg_id" {
+  value = aws_security_group.elasticsearch_clients_security_group.*.id[0]
+}
+
 output "vm_password" {
   value = random_string.vm-login-password.result
 }
